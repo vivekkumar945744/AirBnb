4e7d4cb265511ae20c473a01ebedf11f3dd3ddb9
vivekkumar945744 <vivekkumar945744@gmail.com>
2025-11-30 10:34:08 +0530

    first commit

---
+++ src/main/java/com/vivekkumar/projects/airBnbApp/AirBnbAppApplication.java
@@ -0,0 +1,13 @@
+package com.vivekkumar.projects.airBnbApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirBnbAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirBnbAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vivekkumar/projects/airBnbApp/AirBnbAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vivekkumar.projects.airBnbApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirBnbAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
